4ba179991e2d23e789ce75300cd57d005e187a4a
Shubham bansal <shubhambansal05@gmail.com>
2026-04-25 15:37:43 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for(int i=0; i<ans.length; i++)
+            ans[i] = i < nums.length ? nums[i] : nums[i-nums.length];
+        return ans;
+    }
+}
